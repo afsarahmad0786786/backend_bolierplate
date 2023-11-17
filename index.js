a152c589e53c179b155157cd25cb9ba461edf68c
Afsar Shaikh <afsar.shaikh@v2solutions.com>
2023-11-17 16:21:02 +0530

    Added swagger for user routes

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const UserRoute = require('./routes/user')
 const EmployeeRoute = require('./routes/employee')
-const connection = require('./connection')
+const connection = require('./connection');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+		info: {
+            title: "Backend Boiler plate",
+			version: "1.0.0",
+			description: "A simple Express Server with SignIn and Sign Up and CRUD Operation on employee table",
+		},
+		servers: [
+            {
+                url: "http://localhost:8080",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 const app = express();
 
-// app.listen(process.env.PORT, async () => {   console.log(`server up on port ${process.env.PORT}`); });
-app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+// app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const logger = require('./logger/bunyanLogger.js').child({
     module: 'NodeJS boilerplate accelerator'
 })
 
+
+
 app.listen(process.env.PORT, () => {
     logger.info('Server Connected', process.env.PORT)
 })
+
